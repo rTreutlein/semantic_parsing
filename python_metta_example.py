fd4faf1140c3051516b7debf32edd5fe8532ad00
Roman Treutlein <roman@trueagi.io>
2024-08-05 10:20:21 +0200

    fix: Refactor the Python MeTTa example

--- python_metta_example.py
+++ python_metta_example.py
@@ -2,77 +2,30 @@ from hyperon import MeTTa
 import random
 import string
 
+def run_metta_from_file(file_path, metta):
+    with open(file_path, 'r') as file:
+        chainerstringhere = file.read()
+        metta.run(chainerstringhere)
+
 metta = MeTTa()
 
-metta.run('''
+# Load the chainer.metta file
+run_metta_from_file('chainer.metta', metta)
 
-(: Nat Type)
-(: Z Nat)
-(: S (-> Nat Nat))
+# Create a new knowledge base
+metta.run('!(bind! &kb (new-space))')
 
-;; Define <=
-(: <= (-> $a $a Bool))
-(= (<= $x $y) (or (< $x $y) (== $x $y)))
+# Load the rules.metta file into the knowledge base
+run_metta_from_file('rules.metta', metta)
 
-;; Define cast functions between Nat and Number
-(: fromNumber (-> Number Nat))
-(= (fromNumber $n) (if (<= $n 0) Z (S (fromNumber (- $n 1)))))
-(: fromNat (-> Nat Number))
-(= (fromNat Z) 0)
-(= (fromNat (S $k)) (+ 1 (fromNat $k)))
-
-
-
-(: bc (-> $a                            ; Knowledge base space
-          $b                            ; Query
-          Nat                           ; Maximum depth
-          $b))                          ; Result
-;; Base case
-(= (bc $kb (: $prf $ccln) $_) (match $kb (: $prf $ccln) (: $prf $ccln)))
-;; Recursive step
-(= (bc $kb (: ($prfabs $prfarg) $ccln) (S $k))
-   (let* (((: $prfabs (-> $prms $ccln)) (bc $kb (: $prfabs (-> $prms $ccln)) $k))
-          ((: $prfarg $prms) (bc $kb (: $prfarg $prms) $k)))
-     (: ($prfabs $prfarg) $ccln)))
-
-(: fc (-> $a                            ; Knowledge base space
-          $b                            ; Source
-          Nat                           ; Maximum depth
-          $b))                          ; Conclusion
-;; Base case
-(= (fc $kb (: $prf $prms) $_) (: $prf $prms))
-;; Recursive step
-(= (fc $kb (: $prfarg $prms) (S $k))
-   (let (: $prfabs (-> $prms $ccln)) (bc $kb (: $prfabs (-> $prms $ccln)) $k)
-     (fc $kb (: ($prfabs $prfarg) $ccln) $k)))
-(= (fc $kb (: $prfabs (-> $prms $ccln)) (S $k))
-    (let (: $prfarg $prms) (bc $kb (: $prfarg $prms) $k)
-     (fc $kb (: ($prfabs $prfarg) $ccln) $k)))
-''')
-
-
-
-atoms = ["A","(→ A B)","(→ B C)"]
 
 def generate_random_identifier(length=8):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
-metta.run('!(bind! &kb (new-space))')
-
-
-metta.run('''
-!(add-atom &kb (: ModusPonens
-                (-> (→ $p $q)           ; Premise 1
-                    (-> $p              ; Premise 2
-                        $q))))          ; Conclusion
-
-''')
-
-for atom in atoms:
+def add_atom_and_run_fc(atom, metta):
     identifier = generate_random_identifier()
     res = metta.run('!(add-atom &kb (: ' + identifier + ' ' + atom + '))')
-    print(res)
 
     res = metta.run('!(fc &kb (: ' + identifier + ' ' + atom + ') (fromNumber 3))')
     print(res)
