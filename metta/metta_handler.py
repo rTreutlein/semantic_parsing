b6986ed9caae6c2b5aba650a859dcb2f918fc3dd
Your Name <you@example.com>
2024-08-28 07:34:33 +0000

    Rename python_metta_exampele to metta_handler
    Add save and restore capabilty (not final)

---
+++ metta/metta_handler.py
@@ -0,0 +1,53 @@
+from hyperon import MeTTa                                                    
+import random                                                                
+import string                                                                
+import json                                                                  
+                                                                             
+class MeTTaHandler:                                                          
+    def __init__(self):                                                      
+        self.metta = MeTTa()                                                 
+        self.run_metta_from_file('metta/Num.metta')                          
+        self.run_metta_from_file('metta/Intersection.metta')                 
+        self.run_metta_from_file('metta/sythesize.metta')                    
+        self.run_metta_from_file('metta/rules_pln.metta')                    
+                                                                             
+    def run_metta_from_file(self, file_path):                                
+        with open(file_path, 'r') as file:                                   
+            chainerstringhere = file.read()                                  
+            self.metta.run(chainerstringhere)                                
+                                                                             
+    @staticmethod                                                            
+    def generate_random_identifier(length=8):                                
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=length))                                                                   
+                                                                             
+    def add_atom_and_run_fc(self, atom):                                     
+        identifier = self.generate_random_identifier()                       
+        self.metta.run(f'(= (kb) (: {identifier} {atom}))')                  
+        res = self.metta.run('!(fc kb rb)')                                  
+        out = [str(elem.get_children()[2]) for elem in res[0]]               
+        return out                                                           
+                                                                             
+    def run(self, atom):                                                     
+        res = self.metta.run(atom)                                           
+        return res                                                           
+                                                                             
+    def store_kb_to_file(self, filename):                                    
+        kb_content = self.metta.run('!(kb)')                                  
+        with open(filename, 'w') as f:                                       
+            json.dump(str(kb_content), f)                                    
+                                                                             
+    def load_kb_from_file(self, filename):                                   
+        with open(filename, 'r') as f:                                       
+            kb_content = json.load(f)                                        
+        self.metta.run(f'(= (kb) {kb_content})')                             
+                                                                             
+if __name__ == "__main__":                                                   
+    handler = MeTTaHandler()                                                 
+    # Example usage                                                          
+    result = handler.add_atom_and_run_fc('(ImplicationLink (PredicateNode take_care_of) (PredicateNode last_longer))')                               
+    result = handler.add_atom_and_run_fc('(ImplicationLink (PredicateNode last_longer) (AndLink (PredicateNode requires_less_frequent_replacement) (PredicateNode requires_less_frequent_maintenance) ) )')
+    print(result)                                                            
+                                                                             
+    # Example of storing and loading KB                                      
+    handler.store_kb_to_file('kb_backup.json')                               
+    handler.load_kb_from_file('kb_backup.json')
